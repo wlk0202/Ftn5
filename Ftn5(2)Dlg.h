2ea10c04598171b6e1e38e308c5fa97bda1cff31
wlk0202 <54826094+wlk0202@users.noreply.github.com>
2019-12-17 11:08:34 +0900

    Add files via upload

---
+++ Ftn5(2)Dlg.h
@@ -0,0 +1,37 @@
+﻿
+// Ftn5(2)Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CFtn52Dlg 대화 상자
+class CFtn52Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CFtn52Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FTN52_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnEnChangeEdit3();
+	afx_msg void OnBnClickedButton1();
+};
